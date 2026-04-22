1c06f2582d93a99408b38eb69e734f972095d6bc
al24057 <yushitennis3@icloud.com>
2026-04-23 02:23:14 +0900

    Create some pages

--- Lost_found_System/Lost_found_Web/urls.py
+++ Lost_found_System/Lost_found_Web/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = "Lost_found_Web"
 urlpatterns = [
     path("", views.index, name="index"),
+    path('post/', views.post, name='post'),
+    path('search/', views.search, name='search'),
+    path('history/', views.history, name='history'),
 ]
--- Lost_found_System/Lost_found_Web/views.py
+++ Lost_found_System/Lost_found_Web/views.py
@@ -5,5 +5,20 @@ class IndexView(View):
     def get(self, request):
         return render(request, "Lost_found_Web/index.html")
     
+class PostView(View):
+    def get(self, request):
+        return render(request, "Lost_found_Web/post.html")
+    
+class SearchView(View):
+    def get(self, request):
+        return render(request, "Lost_found_Web/search.html")
+    
+class HistoryView(View):
+    def get(self, request):
+        return render(request, "Lost_found_Web/history.html")
+    
     
 index = IndexView.as_view()
+post = PostView.as_view()
+search = SearchView.as_view()
+history = HistoryView.as_view()
