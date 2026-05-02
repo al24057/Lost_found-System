4197e7ee2ca7da7798f0a4523e7323957441bbe2
al24057 <yushitennis3@icloud.com>
2026-05-02 16:05:51 +0900

    Create responsive_design and favicon

--- Lost_found_System/static/Lost_found_Web/homes.js
+++ Lost_found_System/static/Lost_found_Web/homes.js
@@ -1,5 +1,14 @@
 let targetUrl = '';
 
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const overlay = document.getElementById('overlay');
+    
+    // クラスの付け外しで表示・非表示を切り替える
+    sidebar.classList.toggle('active');
+    overlay.classList.toggle('active');
+}
+
 function openConfirm(url) {
   targetUrl = url;
   document.getElementById('modal').style.display = 'flex';
