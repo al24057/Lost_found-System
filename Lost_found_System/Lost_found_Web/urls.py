5153fe6bbfe6a50f275f5e5d3c7a08904c5c1b2e
101-mitsuki <al24101@shibaura-it.ac.jp>
2026-06-06 16:36:18 +0900

    履歴ページ作成

--- Lost_found_System/Lost_found_Web/urls.py
+++ Lost_found_System/Lost_found_Web/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('detail/<int:pk>/', views.detail, name='detail'),
     path('post/', views.post, name='post'),
     path('search/', views.search, name='search'),
+    path('post/edit/<int:pk>/', views.post, name='post_edit'),
     path('history/', views.history, name='history'),
 ]
