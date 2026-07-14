5111d03f61216842a61b322cdd7c86ea05ec0209
al24057 <yushitennis3@icloud.com>
2026-07-14 14:42:07 +0900

    Create super user

--- Lost_found_System/Lost_found_System/wsgi.py
+++ Lost_found_System/Lost_found_System/wsgi.py
@@ -10,7 +10,22 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import django
+from django.contrib.auth import get_user_model
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Lost_found_System.settings')
 
 application = get_wsgi_application()
+
+# Djangoの設定とアプリの読み込みを確実に完了させる
+django.setup()
+
+User = get_user_model()
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if username and password:
+    if not User.objects.filter(username=username).exists():
+        # メールアドレスは空文字 "" で作成します
+        User.objects.create_superuser(username=username, email="", password=password)
+        print(f"Superuser '{username}' created successfully.")
